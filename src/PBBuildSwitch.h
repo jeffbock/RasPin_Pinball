cd79f9ebc3bc11b7dde62693098a295c358dd9b2
jeffbock <jeffdbock@gmail.com>
2025-04-30 06:54:06 -0700

    Fixing the Windows Build

--- src/PBBuildSwitch.h
+++ src/PBBuildSwitch.h
@@ -1,4 +1,4 @@
 // This final only serves as the build switch between Windows and PiOS
 
-// #define EXE_MODE_WINDOWS
-#define EXE_MODE_RASPI
+#define EXE_MODE_WINDOWS
+// #define EXE_MODE_RASPI
--- src/PBWinRender.h
+++ src/PBWinRender.h
@@ -4,10 +4,8 @@
 #ifndef PBWinRender_h
 #define PBWinRender_h
 
-#ifdef EXE_MODE_WINDOWS
 #include <windows.h>
 
 HWND PBInitWinRender (long width, long height);
-#endif // EXE_MODE_WINDOWS
 
 #endif // PBWinRender_h
