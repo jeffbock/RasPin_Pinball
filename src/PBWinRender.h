2bd95ccf6dcd5a7bb21e0ad2d087d5fa22300d79
jdbock <jeffdbock@gmail.com>
2025-04-30 07:05:08 -0700

    More minor build fixes

--- src/PBWinRender.h
+++ src/PBWinRender.h
@@ -1,6 +1,8 @@
 // Window and render code for Windows
 // MIT License, Copyright (c) 2025 Jeffrey D. Bock, except where where otherwise noted
 
+// Note: This file is not built during the RasPi build flow
+
 #ifndef PBWinRender_h
 #define PBWinRender_h
 
