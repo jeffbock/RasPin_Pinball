e641a49d25b14c2a24ad13dd81eb50dda742b1ef
jdbock <jeffdbock@gmail.com>
2025-09-28 22:15:28 -0700

    Fixing Raspberry Pi build

--- src/PBBuildSwitch.h
+++ src/PBBuildSwitch.h
@@ -7,7 +7,7 @@
 
 // This header file only serves as the build switch between Windows and PiOS.  Uncomment the appropriate linefor the desired platform.
 
-#define EXE_MODE_WINDOWS
-// #define EXE_MODE_RASPI
+//#define EXE_MODE_WINDOWS
+#define EXE_MODE_RASPI
 
 #endif
